bb2495fca03984a540d3cfab9a32ccf4527b5517
BloxWarden <gamingmaster376w@gmail.com>
2026-05-01 18:35:43 +0530

    Add files via upload

---
+++ follow.py
@@ -0,0 +1,14 @@
+import pgzrun
+
+from random import randint
+
+WIDTH = 400
+HEIGHT = 400
+
+dots = []
+lines = []
+
+nextdot = 0
+
+for dot in range(0, 10):
+    actor = Actor()
---
+++ shoot.py
@@ -0,0 +1,26 @@
+import pgzrun
+
+from random import randint
+
+apple = Actor("apple")
+
+def draw():
+    screen.clear()
+    apple.draw()
+
+def place_apple():
+    apple.x = randint(10, 800)
+    apple.y = randint(10, 600)
+
+def on_mouse_down(pos):
+    if apple.collidepoint(pos):
+        print("Good Shot!")
+        place_apple()
+    else:
+        print("You Missed!")
+        place_apple()
+    
+
+place_apple()
+
+pgzrun.go()    
